99538f5eed4b8e073823185ee35b40925188e1bf
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2021-09-30 21:08:30 +0200

    tests goto

--- odometry.py
+++ odometry.py
@@ -35,7 +35,7 @@ try:
     i = 0
     j = 1
     while T > 0:
-        # i += 1
+        i += 1
         w_r, w_l = dxl_io.get_present_speed(ids)
         w_r, w_l = - w_r * (np.pi / 180), w_l * (np.pi / 180)
         v, w = direct_kinematics(w_l, w_r)
