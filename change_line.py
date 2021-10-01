20536e9a3a9e05891fd28c2e7bf6d0998aef883d
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2021-10-01 13:30:29 +0200

    Matin

--- change_line.py
+++ change_line.py
@@ -146,8 +146,8 @@ try:
                 )
             else:
                 speed = (
-                    - RED_SPEED + 0.6 * x + 0.0002 * (x / abs(x)) * x ** 2,
-                    RED_SPEED + 0.6 * x + 0.0002 * (x / abs(x)) * x ** 2
+                    - RED_SPEED + 0.6 * x + 0.0003 * (x / abs(x)) * x ** 2,
+                    RED_SPEED + 0.6 * x + 0.0003 * (x / abs(x)) * x ** 2
                 )
 
             dxl_io.set_moving_speed({
