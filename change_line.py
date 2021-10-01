2207ceaba4d27a9f81213fcf203cd2d0218d7282
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2021-10-01 12:24:19 +0200

    Matin

--- change_line.py
+++ change_line.py
@@ -14,16 +14,8 @@ from image_processing import processing, init_values
 #         sum_2 += list_speed[i][1]
 #     return sum_1/l, sum_2/l
 
-FIRST_LOOP = 2
-
-line_color = "blue"
-nb_args = len(sys.argv)
-if nb_args > 1:
-    line_color = sys.argv[1]
-
-default_speed, p = init_values(line_color)
-
 ports = pypot.dynamixel.get_available_ports()
+
 if not ports:
     exit('No port')
 
@@ -39,8 +31,6 @@ print('Found ids:', found_ids)
 ids = found_ids[:2]
 dxl_io.enable_torque(ids)
 
-speed = (-default_speed, default_speed)
-
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
@@ -52,6 +42,8 @@ change_line = False
 initial_time = 0
 
 try:
+    default_speed, p = init_values("blue")
+    speed = (-default_speed, default_speed)
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -61,21 +53,36 @@ try:
         x, y, change = processing(frame)
         print("x, y :", x, y)
 
-        """if change and not change_line:
-            change_line = True
-            # dxl_io.set_moving_speed({1:0, 2:0})
-            if line_color == "blue":
-                line_color = "red"
-                default_speed, p = init_values("red")
-                initial_time = time.time()
-            else:
-                line_color = "blue"
-                default_speed, p = init_values("blue")
+        if x == None:
+            print("No Line")
+            dxl_io.set_moving_speed({
+                1: speed[0],
+                2: speed[1]
+            })
 
-        elif not change:
-            change_line = False"""
+        else:
+            speed = (
+                - default_speed + 0.55 * x,
+                default_speed + 0.55 * x
+            )
+            print(x)
+            print(speed)
 
-        # print(line_color)
+            dxl_io.set_moving_speed({
+                1: speed[0],
+                2: speed[1]
+            })
+
+    default_speed, p = init_values("red")
+    speed = (-default_speed, default_speed)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?).")
+            continue
+
+        x, y, change = processing(frame)
+        print("x, y :", x, y)
 
         if x == None:
             print("No Line")
@@ -84,19 +91,10 @@ try:
                 2: speed[1]
             })
 
-        #     print("No line detected.")
-        #     s = 40
-        #     mean_speed = {
-        #         1: -default_speed + s,
-        #         2: default_speed + s
-        #     }
-        #     dxl_io.set_moving_speed(mean_speed)
-        #
-        # else:
         else:
             speed = (
-                - default_speed + 0.5 * x,
-                default_speed + 0.5 * x
+                - default_speed + 0.55 * x,
+                default_speed + 0.55 * x
             )
             print(x)
             print(speed)
